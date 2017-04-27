17b020aab6ac3b20967b93a3c6211d59a0d17ed7
luispaulotfv <luispaulotfv@gmail.com>
2017-04-27 14:57:01 +0100

    Add files via upload

---
+++ js/indexController.js
@@ -0,0 +1,198 @@
+
+
+'use strict;'
+
+// application
+var app = {
+    isLoading: true,
+    packageList: [],
+    localPackageList: [],
+    spinner: document.querySelector('.loader'),
+    listContainer: document.querySelector('.packageList'),
+    detailContainer: document.querySelector('.packageDetail')
+};
+
+// event listener's when click refresh
+document.getElementById('btnRefresh').addEventListener('click', function() {
+    app.getAllPackages();
+});
+
+// get all packages if have net from server else from local storage
+app.getAllPackages = function() {
+    if (navigator.onLine) {
+        console.log("[getAllPackages] from server");
+        app.getAllPackagesFromServer();
+    } else {
+        console.log("[getAllPackages] from local");
+        app.getAllPackagesFromLocal();
+    }
+}
+
+// get all packages from Server
+app.getAllPackagesFromServer = function() {
+    var requestUrl = 'js/data.json';
+    
+    // TODO add cache logic here
+    if ('caches' in window) {
+      /*
+       * Check if the service worker has already cached data.
+       * If the service worker has the data, then display the cached
+       * data while the app fetches the latest data.
+       */
+        caches.match(requestUrl).then(function(response) {
+            if (response) {
+                response.json().then(function loadFromCache(itemsInCache) {
+                    packageList = itemsInCache;
+                    app.showAllPackages();
+                });
+            }
+        });
+    }
+    
+    var client = new HttpClient();
+    client.get(requestUrl, function(response) {
+        if (response && response.length > 0) {
+            // server send data
+            packageList = JSON.parse(response); 
+            app.saveLocalPackageList();
+        } else {
+            // server send NO data try local
+            packageList = [];
+            app.getAllPackagesFromLocal();
+        }
+        app.showAllPackages();
+    });
+};
+
+// get package list from local storage
+app.getAllPackagesFromLocal = function() {
+    if (localStorage.localPackageList) {
+        packageList = JSON.parse(localStorage.localPackageList);
+        app.showAllPackages();
+    }
+};
+
+// get a package by id
+app.getPackage = function(id) {
+    for (i = 0; i < packageList.length; i++) {
+        var package = packageList[i];
+        if (package.id == id) {
+            return package;
+        }
+    }
+};
+
+// save packages list in local storage
+app.saveLocalPackageList = function() {
+    var packages = JSON.stringify(packageList);
+    localStorage.localPackageList = packages;
+};
+
+// display all packages in view
+app.showAllPackages = function() {
+    app.clearView();
+    app.showSpinner();
+    var html = "";
+    for (i = 0; i < packageList.length; i++) {
+        var package = packageList[i];
+        html += app.getMiniPackageHtml(package);
+    }
+    app.hideSpinner();
+    app.listContainer.innerHTML = html;
+    scrollTop();
+};
+
+// click package details
+var clickShowPackage = function(id) {
+    app.clearView();
+    app.showSpinner();
+    var package = app.getPackage(id);
+    app.hideSpinner();
+    app.detailContainer.innerHTML = app.getPackageHtml(package);
+    scrollTop();
+};
+
+// package html summary
+app.getMiniPackageHtml = function(package) {
+    var html = "";
+    if (package) {
+        html = '<div class="miniPackageTemplate shadow" onclick="clickShowPackage(' + "'" + package.id + "'" + ')">'
+        html += '<img src="' + package.img + '" alt="' + package.name + '" class="img-responsive">';
+        html += '<h5>' + package.name + '</h5>';
+        html += '<div class="packageDate text-info">' + package.date + '</div>';
+        html += '</div>';
+    }
+    return html;
+};
+
+// package html detail
+app.getPackageHtml = function(package) {
+    var html = "";
+    if (package) {
+        html = '<div class="packageTemplate shadow">'
+        html += '<img src="' + package.img + '" alt="' + package.name + '" class="img-responsive">';
+        html += '<h4>' + package.name + '</h4>';
+        html += '<div><strong>' + package.id + '</strong></div>';
+        html += '<div class="packageDescription"><p>' + package.description + '</p></div>';
+        if (package.includedServices) {
+            html += '<h5>Serviços Incluidos</h5>';
+            html += '<div class="includedServices">' + package.includedServices + '</div><br>'
+        }
+        html += '<div class="packageDate text-info">' + package.date + '</div>';
+        html += '<div class="packagePrice text-danger text-right"><strong>' + Number(package.price).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,') + ' €</strong></div>';
+        html += '<div class="clearFloats"></div>';
+        html += '</div>';
+    }
+    return html;
+};
+
+// show spinner
+app.showSpinner = function() {
+    app.spinner.setAttribute('hidden', false);
+    app.isLoading = true;
+};
+
+// hide spinner
+app.hideSpinner = function() {
+    app.spinner.setAttribute('hidden', true);
+    app.isLoading = false;
+};
+
+// clear view's html
+app.clearView = function() {
+    app.listContainer.innerHTML = "";
+    app.detailContainer.innerHTML = "";
+    app.showSpinner();
+};
+
+// Http client async
+var HttpClient = function() {
+    this.get = function(aUrl, aCallback) {
+        var anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function() { 
+            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                aCallback(anHttpRequest.responseText);
+        }
+        // set false for synchronous request, synchronous requests are discouraged
+        // use true for asynchronous  
+        anHttpRequest.open( "GET", aUrl, true );            
+        anHttpRequest.send( null );
+    }
+};
+
+// move win to top
+var scrollTop = function() {
+    window.scrollTo(0, 0);
+};
+
+// app start
+app.getAllPackages();
+
+// verificar se o navegador oferece suporte a service workers e, em caso positivo, registrar o service worker
+if  ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('./service-worker.js')
+        .then(function() {  
+            console.log('Service Worker Registered');
+        });
+}
---
+++ service-worker.js
@@ -0,0 +1,74 @@
+
+
+var cacheName = 'ProgressiveTriPointApp-v1';
+var dataCacheName = 'PackagesData-v1';
+var filesToCache = [
+    '/',
+    '/index.html',
+    '/js/indexController.js',
+    '/css/bootstrap.min.css',
+    '/css/site.css',
+    '/images/ic_refresh_white_24px.svg',
+    '/images/LogoTFVsaFinal-médio.png'
+];
+
+// install
+self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Install. (event)', e);
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            console.log('[ServiceWorker] Caching app shell. (cache)', cache);
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+// activate
+self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activate. (event)', e);
+    e.waitUntil(
+        caches.keys().then(function(keyList) {
+            console.log('[ServiceWorker] Caches. (keyList)', keyList);
+            return Promise.all(keyList.map(function(key) {
+                if(key !== cacheName && key !== dataCacheName) {
+                    console.log('[ServiceWorker] Removing old cache (key)', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
+
+// fetch
+self.addEventListener('fetch', function(e) {
+    console.log('[ServiceWorker] Fetch. (event)', e);
+    var dataUrl = 'js/data.json';
+    if (e.request.url.indexOf(dataUrl) > -1) {
+        /*
+        * When the request URL contains dataUrl, the app is asking for fresh
+        * data. In this case, the service worker always goes to the
+        * network and then caches the response. This is called the "Cache then
+        * network" strategy:
+        * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
+        */
+        e.respondWith(
+            caches.open(dataCacheName).then(function(cache) {
+                return fetch(e.request).then(function(response){
+                cache.put(e.request.url, response.clone());
+                return response;
+                });
+            })
+        );
+    } else {
+        /*
+        * The app is asking for app shell files. In this scenario the app uses the
+        * "Cache, falling back to the network" offline strategy:
+        * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
+        */
+        e.respondWith(
+            caches.match(e.request).then(function(response) {
+                return response || fetch(e.request);
+            })
+        );
+    }
+});
